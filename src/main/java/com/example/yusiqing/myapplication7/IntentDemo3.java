430be2d8f957d0b66d6488363693f1419b898ccf
07150946 <344579964@qq.com>
2016-12-12 14:23:14 +0800

    q

---
+++ src/main/java/com/example/yusiqing/myapplication7/IntentDemo3.java
@@ -0,0 +1,16 @@
+package com.example.yusiqing.myapplication7;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yusiqing on 2016/10/27.
+ */
+public class IntentDemo3 extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
